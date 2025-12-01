470479d752d25533c35ed5ca8c5708560418909d
Nikoloz Kavtaradze <nikoloz.k@onthegosystems.com>
2025-12-01 10:22:55 +0400

    Step 8: Create a service that calls the PTC API

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   extend Mobility
   translates :title, type: :string
   translates :description, type: :text
+
+  after_create :send_for_translation
+
+  private
+
+  def send_for_translation
+    Ptc::TranslateService.call(data: { title: title, description: description }, name: title, target_languages: ["fr", "de"])
+  end
 end
---
+++ app/services/ptc/translate_service.rb
@@ -0,0 +1,53 @@
+require 'net/http'
+require 'uri'
+
+module Ptc
+  class TranslateService
+    def initialize(data:, name: ,target_languages: )
+      @data = data
+      @name = name
+      @target_languages = target_languages
+      @token = ENV.fetch("PTC_API_TOKEN")
+    end
+
+    def call
+      translate
+    end
+
+    def self.call(**attributes)
+      new(**attributes).call
+    end
+
+    private
+    attr_reader :data, :name, :target_languages, :token
+
+    def translate
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(request)
+      end
+      JSON.parse(response.body)
+    end
+
+    def body
+      {
+        data:,
+        name:,
+        target_languages:,
+      }.to_json
+    end
+
+    def request
+      return @request if @request.present?
+
+      @request ||= Net::HTTP::Post.new(uri)
+      @request.content_type = "application/json"
+      @request.body = body
+      @request["Authorization"] = "Bearer #{token}"
+      @request
+    end
+
+    def uri
+      @uri ||= URI.parse("https://app.ptc.wpml.org/api/v1/content_translation")
+    end
+  end
+end
