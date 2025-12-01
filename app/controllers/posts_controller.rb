987876f1ad1bcc4225dc31146694b057e2e533fb
Nikoloz Kavtaradze <nikoloz.k@onthegosystems.com>
2025-12-01 10:15:36 +0400

    Step 5: Modify views to support editing translations (for current locale)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:published)
+      params.require(:post).permit(:published, :title, :description)
     end
 end
