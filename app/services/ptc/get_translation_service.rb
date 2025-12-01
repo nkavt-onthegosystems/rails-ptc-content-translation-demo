d5a490a8043eff72de15c8b215d1368a3fc294c1
Nikoloz Kavtaradze <nikoloz.k@onthegosystems.com>
2025-12-01 10:26:19 +0400

    Step 11: Add services to get status and translation

---
+++ app/services/ptc/get_translation_service.rb
@@ -0,0 +1,42 @@
+require 'net/http'
+require 'uri'
+
+module Ptc
+  class GetTranslationService
+    def initialize(id:)
+      @id = id
+      @token = ENV.fetch("PTC_API_TOKEN")
+    end
+
+    def call
+      get
+    end
+
+    def self.call(**attributes)
+      new(**attributes).call
+    end
+
+    private
+    attr_reader :id, :token
+
+    def get
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(request)
+      end
+        JSON.parse(response.body)
+    end
+
+    def request
+      return @request if @request.present?
+
+      @request ||= Net::HTTP::Get.new(uri)
+      @request.content_type = "application/json"
+      @request["Authorization"] = "Bearer #{token}"
+      @request
+    end
+
+    def uri
+      @uri ||= URI.parse("https://app.ptc.wpml.org/api/v1/content_translation/#{id}")
+    end
+  end
+end
